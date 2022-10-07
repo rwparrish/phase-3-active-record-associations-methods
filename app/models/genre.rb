253adb34059c0bce267f2e05caad15261f333bcb
rwparrish <60946750+rwparrish@users.noreply.github.com>
2022-10-07 17:01:24 -0600

    commit tree 3316c4ac85b0f5cba68f58b4e94bfb084ba578b8

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,16 +1,21 @@
+require 'pry'
+
 class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
 
   def song_count
+    self.songs.count
     # return the number of songs in a genre
   end
 
   def artist_count
+    self.artists.count
     # return the number of artists associated with the genre
   end
 
   def all_artist_names
+    Artist.all.map { |artist| artist.name }
     # return an array of strings containing every musician's name
   end
 end
